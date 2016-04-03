b6025d3712d5b0aa867432696d9ac39945e664e4
Matt G <matt.gaikema@tamu.edu>
2016-04-03 13:19:39 -0500

    Add member variables

--- app/src/main/java/com/example/mgaik/gaikr/GaikrActivity.java
+++ app/src/main/java/com/example/mgaik/gaikr/GaikrActivity.java
@@ -2,8 +2,15 @@ package com.example.mgaik.gaikr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class GaikrActivity extends AppCompatActivity {
+    // Declare our view variables
+
+    private Button mNextButton;
+    private Button mPrevButton;
+    private FrameLayout mImgDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
